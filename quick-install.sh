ba0cbc9ecc01c2526417a8e04090807f5a0bbf35
robtweed <rtweed@mgateway.com>
2019-11-05 16:09:22 +0000

    Quick installer now creates the Docker networks if needed

--- quick-install.sh
+++ quick-install.sh
@@ -12,5 +12,10 @@ echo 'If you are running this for the first time, the Node-Runner Container'
 echo 'will first be downloaded from Docker Hub.'
 echo 'If so, please wait for it to load'
 
+# Create the Docker networks unless they already exist
+
+docker network ls|grep qewd-hit > /dev/null || docker network create qewd-hit
+docker network ls|grep ecis-net > /dev/null || docker network create ecis-net
+
 docker run -it --name installer --rm -v $VOLUME:/node -e "node_script=quick-install" -e "DOCKER_HOST=$(ip -4 addr | grep -Po 'inet \K[\d.]+')" -e "HOST_VOLUME=$VOLUME" rtweed/node-runner
 
